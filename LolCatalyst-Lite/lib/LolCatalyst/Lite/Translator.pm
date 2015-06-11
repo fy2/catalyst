44681dff2b57ee77749e7b5d7b80666cf18fdcd9
Feyruz Yalcin <feyruzyalcin@gmail.com>
2015-06-11 21:43:33 +0100

    LOLCAT.pm in its own package file now

--- LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
+++ LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
@@ -1,6 +1,7 @@
 package LolCatalyst::Lite::Translator;
 
 use Moose;
+use LolCatalyst::Lite::Translator::LOLCAT;
 use namespace::clean -excep => 'meta';
 
 has 'default_target' => (
@@ -26,47 +27,6 @@ sub translate_to {
   my ($self, $target, $text) = @_;
   $self->translators->{$target}->translate($text);
 }
-
-
-package LolCatalyst::Lite::Translator::LOLCAT;
-use Moose;
-use namespace::autoclean;
-use Acme::LOLCAT ();
-
-extends 'Catalyst::Model';
-
-=head1 NAME
-
-LolCatalyst::Lite::Translator - Catalyst Model
-
-=head1 DESCRIPTION
-
-Catalyst Model.
-
-=head2 translate
-
-translate a text into lolcat language
-
-=cut
-
-sub translate {
-  my ($self, $text) = @_;
-
-  return Acme::LOLCAT::translate($text);
-}
-
-=encoding utf8
-
-=head1 AUTHOR
-
-fy,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 __PACKAGE__->meta->make_immutable;
 
