7794569a713038e6b521af02b2a3846e72b1b07b
Feyruz Yalcin <feyruzyalcin@gmail.com>
2015-06-11 21:11:35 +0100

    [88] Driver model for generic translation

--- LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
+++ LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
@@ -1,4 +1,34 @@
 package LolCatalyst::Lite::Translator;
+
+use Moose;
+use namespace::clean -excep => 'meta';
+
+has 'default_target' => (
+  is => 'ro', isa => 'Str', required => 1, default => 'LOLCAT'
+);
+
+
+has 'translators' => (
+  is => 'ro', isa => 'HashRef', lazy_build => 1
+);
+
+sub _build_translators {
+  my ($self) = @_;
+  return { LOLCAT => LolCatalyst::Lite::Translator::LOLCAT->new};
+}
+
+sub translate {
+  my ($self, $text) = @_;
+  $self->translate_to($self->default_target, $text);
+}
+
+sub translate_to {
+  my ($self, $target, $text) = @_;
+  $self->translators->{$target}->translate($text);
+}
+
+
+package LolCatalyst::Lite::Translator::LOLCAT;
 use Moose;
 use namespace::autoclean;
 use Acme::LOLCAT ();
