7d8c74e98aa336832348bba3a0d213c72fe094d1
Feyruz Yalcin <feyruzyalcin@gmail.com>
2015-06-11 22:14:14 +0100

    [90, 91) Using a pluggable driver system
      * use Module::Pluggable::Object to simplify driver plugins (fantastic)
      * the book uses Class::MOP but that is deprecated, using Class::Load instead

--- LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
+++ LolCatalyst-Lite/lib/LolCatalyst/Lite/Translator.pm
@@ -1,7 +1,7 @@
 package LolCatalyst::Lite::Translator;
 
 use Moose;
-use LolCatalyst::Lite::Translator::LOLCAT;
+use Module::Pluggable::Object;
 use namespace::clean -excep => 'meta';
 
 has 'default_target' => (
@@ -9,13 +9,44 @@ has 'default_target' => (
 );
 
 
-has 'translators' => (
+has '_translators' => (
   is => 'ro', isa => 'HashRef', lazy_build => 1
 );
 
-sub _build_translators {
+sub _build__translators {
   my ($self) = @_;
-  return { LOLCAT => LolCatalyst::Lite::Translator::LOLCAT->new};
+  my $base = __PACKAGE__;
+
+  my $mp = Module::Pluggable::Object->new(
+    search_path => [ $base ]
+  );
+
+  my @classes = $mp->plugins;
+
+  my %translators;
+  foreach my $class (@classes) {
+
+    # Class::MOP is the core metaobject protocol that moose is built on top of,
+    # it provides some handy functions. load_class will work out what to
+    # require() to get the class loaded...  
+    #
+    # !!!! Note !!!! In the original book it was shown as
+    # 'Class::MOP::load_class' but aparently MOP has been deprecated! It is now
+    # Class::Load::load_class instead.....
+    Class::Load::load_class($class); 
+
+    # We copy here the class to name and then strip the base off the front of
+    # the copy in $name. The \Q...\E says that if any regex special chars appear
+    # in the stuff between them, those chars should be ignored (it uses
+    # perldoc -f quotemeta on the demilited chunk basically). This leaves the
+    # $name containing LOLCAT for a $class value of
+    # LolCatalyst::Lite::Translator::LOLCAT
+    (my $name = $class) =~ s/^\Q${base}::\E//;
+
+    $translators{$name} = $class->new;
+  }
+
+  return \%translators;
 }
 
 sub translate {
@@ -25,7 +56,7 @@ sub translate {
 
 sub translate_to {
   my ($self, $target, $text) = @_;
-  $self->translators->{$target}->translate($text);
+  $self->_translators->{$target}->translate($text);
 }
 
 __PACKAGE__->meta->make_immutable;
